bbb33f6f0d0ee58c1c38b521e861f4247eb92d02
Danylo Perepeliuk <danyla958@gmail.com>
2026-02-23 02:49:11 +0200

    Remove unused Axer.ktorPlugin from pluginsList and update environment variables in deploy configuration

--- Jena/src/jvmMain/kotlin/com/oriooneee/jena/koin/AppModule.jvm.kt
+++ Jena/src/jvmMain/kotlin/com/oriooneee/jena/koin/AppModule.jvm.kt
@@ -1,12 +1,9 @@
 package com.oriooneee.jena.koin
 
-import io.github.orioneee.Axer
 import io.ktor.client.plugins.api.ClientPlugin
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 
 actual fun pluginsList(): List<ClientPlugin<out Any>> {
     return listOf(
-            Axer.ktorPlugin,
-            ContentNegotiation
+//            Axer.ktorPlugin,
     )
 }
